725ee6ffa5a0982cc3ff9c0f72655b60ff8b7326
Dalton Whyte <daltino06@yahoo.com>
2019-09-28 17:29:39 +0100

    Merge branch 'master' of https://github.com/spencerlevitt/QadeFrontendWinterFox into development
    # Conflicts:
    #	navigation/AppNavigator.js

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,3 +1,4 @@
 export const environment = {
-  API_URL: 'https://5d88b994feddff0014e15e7f.mockapi.io/api/v1/'
+  MOCK_API_URL: 'https://5d88b994feddff0014e15e7f.mockapi.io/api/v1/',
+  API_URL: 'http://localhost:8000/v1/',    // TODO: CHANGE THIS PROD ENDPOINT
 }
