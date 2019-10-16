382e1dc648ab9d9791a6039f3e0e42f66153cdef
ptalus <pavel.galgan@gmail.com>
2019-10-16 22:32:58 +0300

    expo eject with expokit. Install and link yoti-react-native. Debug build - ok, release build - manifest error

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp0413fcacb37049cc89a9fb9ffe4706d5://192.168.0.106:19000";
+
+}
