8a98e98c3a4a2b642670745d81b5b439d488fc26
ptalus <pavel.galgan@gmail.com>
2019-10-17 23:38:08 +0300

    install tipsi-stripe library

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp0413fcacb37049cc89a9fb9ffe4706d5://192.168.0.106:19000";
+  public static final String DEVELOPMENT_URL = "exp0413fcacb37049cc89a9fb9ffe4706d5://192.168.0.106:80";
 
 }
