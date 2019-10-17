d9150f74a9d4c30837aee977d32a3d1e2bde87a6
ptalus <pavel.galgan@gmail.com>
2019-10-17 21:10:07 +0300

    configuration for release build with yoti.

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ expo start
 cd android
 ./gradlew installDebug
 
+
+#release build
+open project in Android Studio and do Refactor->AndroidX
+run migrating script on node_modules
+put keystore in android/app and set proper keystore params in android/gradle.properties
+cd android
+./gradlew assembleRelease
+#install release
+adb install app/build/outputs/apk/release/app-release.apk
--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -23,9 +23,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@ptalus/Qade", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40ptalus%2FQade%2F1.0.0%2Fb766e8f9013049894f84817087037131-34.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
