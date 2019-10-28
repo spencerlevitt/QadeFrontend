d2097eae9c9add6556cf72c41177171a0b8a6b49
oluwafemi <phemy.smith@gmail.com>
2019-10-28 15:16:56 +0100

    feat(implement pagination)

--- redux/actions/gamersActions.js
+++ redux/actions/gamersActions.js
@@ -2,7 +2,7 @@ import { beginApiCall, apiCallError } from "./apiStatusActions";
 import HttpStatus from 'http-status-codes';
 import { gamers } from '../../api/userApi';
 import { getRecentGamesApi } from '../../api/gameRequestsApi';
-import { FETCH_GAMERS, FETCH_GAMERS_ERROR, FETCH_RECENT_GAMERS, FETCH_RECENT_GAMERS_ERROR } from '../actions/actionTypes';
+import { FETCH_GAMERS, FETCH_GAMERS_ERROR, FETCH_RECENT_GAMERS, FETCH_RECENT_GAMERS_ERROR, REFRESH_RECENT_GAMERS } from '../actions/actionTypes';
 
 export function gamersSearch(payload) {
     return { type: FETCH_GAMERS, payload}
@@ -20,18 +20,28 @@ export function searchGamersError(payload) {
     return { type: FETCH_GAMERS_ERROR, payload}
 }
 
+export function refreshRecentGamers(payload) {
+    return {type: REFRESH_RECENT_GAMERS, payload}
+}
+
 export function getGamers(searchKey, csrfToken) {
     return function (dispatch) {
         dispatch(beginApiCall());
-        return gamers(searchKey, csrfToken).then((data) => dispatch(gamersSearch(data.data)))
-        .catch(error => dispatch(searchGamersError(error)))
+        return gamers(searchKey, csrfToken).then( data => dispatch(gamersSearch(data.data)))
+        .catch(error => dispatch(searchGamersError(error) && console.log(error)))
     }
 }
 
-export function getRecentGames(csrfToken) {
+export function getRecentGames(csrfToken, page, type) {
     return function(dispatch) {
+        console.log(page, "ACTION PAGE")
         dispatch(beginApiCall());
-        return getRecentGamesApi(csrfToken).then( data=> dispatch(recentGamers(data.data.results)))
+        return getRecentGamesApi(csrfToken, page).then( data=> {
+            if(type) {
+                return dispatch(refreshRecentGamers(data.data))
+            }
+            return dispatch(recentGamers(data.data))
+        })
         .catch(error => dispatch(recentGamersError(error)))
     }
 }
